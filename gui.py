b156932ca8dc33ef670c0dcbdf0f90673555ce7f
Scott Jones <34104818+scott2000jones@users.noreply.github.com>
2020-10-31 15:40:25 +0000

    enabled updating of button options

--- gui.py
+++ gui.py
@@ -1,36 +1,85 @@
 from tkinter import *
+import json
 
-class GUI:
-    def __init__(self, master):
-        self.master = master
-        master.title("The Horrors of Practical Submission")
-        master.geometry("600x500")
+# class GUI:
+#
+#     def __init__(self, master):
+#         self.master = master
+#         master.title("Silence of the Labs")
+#         master.geometry("600x500")
+#
+#         textframe = Frame(master)
+#         textframe.place(anchor = "n", relx = 0.5, rely = 0.2)
+#
+#         optionframe = Frame(master)
+#         optionframe.place(anchor = "s", relx = 0.5, rely = 0.8)
+#
+#         f = open("data.json", "r")
+#         datastring = f.read()
+#         x = json.loads(datastring)
+#         print(x)
+#
+#         time = '8:30pm'
+#         currentScene = 'bedroom'
+#         hasMatric = False
+#
+#         self.makeOptions(x['scene'][currentScene], optionframe)
+#
+#         close_button = Button(master, text="Close", command=master.quit)
+#         close_button.place(anchor = "s", relx =  0.5, rely = 1)
+#
+#         submit_button = Button(master, text="Submit", command=self.greet)
+#         submit_button.place(anchor = "s", relx =  0.5, rely = 0.9)
+#
+#     def greet(self, x):
+#         self.makeOptions(x['scene']['Run'])
+#         print("Greetings!")
+#
+#     def makeOptions(self, choices, optionframe):
+#         var = StringVar()
+#         for choice in choices:
+#             if (choice != "prompt"):
+#                 option = Radiobutton(optionframe, text=choice, variable=var, value=choice[1])
+#                 option.pack()
 
-        textframe = Frame(master)
-        textframe.place(anchor = "n", relx = 0.5, rely = 0.2)
 
-        optionframe = Frame(master)
-        optionframe.place(anchor = "s", relx = 0.5, rely = 0.8)
+f = open("data.json", "r")
+datastring = f.read()
+x = json.loads(datastring)
+print(x)
 
-        choices = [["choice 1", "this is where it goes"], ["this is a choice", "it goes here"], ["whoo another choice", "and it goes here"], ["ouioui baguette", "yummy"]]
-        self.makeOptions(choices, optionframe)
+master = Tk()
+master.title("Silence of the Labs")
+master.geometry("600x500")
+textframe = Frame(master)
+textframe.place(anchor = "n", relx = 0.5, rely = 0.2)
+optionframe = Frame(master)
+optionframe.place(anchor = "s", relx = 0.5, rely = 0.8)
 
-
-        close_button = Button(master, text="Close", command=master.quit)
-        close_button.place(anchor = "s", relx =  0.5, rely = 1)
-        
-
-    def greet(self):
-        print("Greetings!")
-
-    def makeOptions(self, choices, optionframe):
-        var = StringVar()
-
-        for choice in choices:
-            option = Radiobutton(optionframe, text=choice[0], variable=var, value=choice[1])
+def makeOptions(choices):
+    optionframe = Frame(master)
+    optionframe.place(anchor = "s", relx = 0.5, rely = 0.8)
+    var = StringVar()
+    for choice in choices:
+        if (choice != "prompt"):
+            option = Radiobutton(optionframe, text=choice, variable=var, value=choice[1])
             option.pack()
 
+def greet():
+    makeOptions(x['scene'][currentScene])
+    print("Greetings!")
 
-root = Tk()
-gui = GUI(root)
-root.mainloop()
+time = '8:30pm'
+currentScene = 'bedroom'
+hasMatric = False
+
+makeOptions(x['scene'][currentScene])
+
+close_button = Button(master, text="Close", command=master.quit)
+close_button.place(anchor = "s", relx =  0.5, rely = 1)
+
+submit_button = Button(master, text="Submit", command=greet)
+submit_button.place(anchor = "s", relx =  0.5, rely = 0.9)
+
+
+mainloop()
